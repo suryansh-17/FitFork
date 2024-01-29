498016939c8f542b3a1a33ea62449300413e24c4
suryansh-17 <suripandey17@gmail.com>
2024-01-29 13:28:27 +0530

    redux added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
-
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
 function App() {
-  const [count, setCount] = useState(0);
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch;
 
-  return (
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
     <>
       <h1 className="underline text-yellow-300 justify-center text-center text-7xl">
         FitFork
       </h1>
     </>
-  );
+  ) : null;
 }
 
 export default App;
